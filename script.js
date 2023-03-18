61e1c082cbd65abf97ca11402e082ad2d586ca35
saroj adhikari <adhikarisaroj291@gmail.com>
2023-03-18 16:37:23 +0545

    adding function

---
+++ script.js
@@ -0,0 +1,24 @@
+// const fbbtn = document.getElementById('fb-btn');
+
+
+
+
+
+// fbbtn.addEventListener('click',gotoLink);
+
+// function gotoLink(event){
+//     event.preventDefault();
+//     const linkElement = document.createElement('a');
+//     linkElement.href = 'https://www.facebook.com/beingsaroj.adk';
+//     // linkElement.target = '_blank';
+//     fbbtn.parentNode.replaceChild(linkElement, fbbtn);
+//     console.log(fbbtn)
+    
+// }
+const whatsApp = document.getElementById('wapp-btn')
+
+whatsApp.addEventListener('click',() =>{
+    
+    alert('9825953356')
+});
+
